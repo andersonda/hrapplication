ea7b3b4e31aadc045a53a379131a1583f2142a36
David <andersonda12@students.ecu.edu>
2017-11-12 14:07:52 -0500

    added display annotations to make labels in profile form more sensible

--- HRApplication/Models/UserProfile.cs
+++ HRApplication/Models/UserProfile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,9 @@ namespace HRApplication.Models
     public class UserProfile
     {
         public string ID { get; set; }
-
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
