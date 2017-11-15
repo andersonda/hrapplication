50b1e0e66d29ef37e4fb64cb83ce0e6b10c2f183
David <andersonda12@students.ecu.edu>
2017-11-14 22:29:55 -0500

    initial work on search for positions functionality

--- HRApplication/Models/PositionContext.cs
+++ HRApplication/Models/PositionContext.cs
@@ -15,5 +15,48 @@ namespace HRApplication.Models
 
         public DbSet<Position> Position { get; set; }
         public DbSet<Application> Application { get; set; }
+
+        public List<Position> getPositionsByCriteria(string column, string criteria)
+        {
+            string searchColumn = column.ToLower();
+            string searchCriteria = criteria.ToLower();
+            List<Position> res = new List<Position>();
+
+            foreach (Position position in Position.ToList())
+            {
+                switch (searchColumn)
+                {
+                    case "id":
+                        var pos = Position.Find(criteria);
+                        if (pos != null)
+                        {
+                            res.Add(pos);
+                        }
+                        break;
+                    case "location":
+                        if (position.Location.ToLower().Contains(searchCriteria))
+                        {
+                            res.Add(position);
+                        }
+                        break;
+                    case "title":
+                        if (position.Title.ToLower().Contains(searchCriteria))
+                        {
+                            res.Add(position);
+                        }
+                        break;
+                    case "description":
+                        if (position.Description.ToLower().Contains(searchCriteria))
+                        {
+                            res.Add(position);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            return res;
+        }
     }
 }
--- HRApplication/Pages/Positions/Index.cshtml.cs
+++ HRApplication/Pages/Positions/Index.cshtml.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HRApplication.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace HRApplication.Pages.Positions
 {
@@ -19,10 +21,40 @@ namespace HRApplication.Pages.Positions
         }
 
         public IList<Position> Position { get;set; }
+        public IList<SelectListItem> SearchCriteria { get; set; }
+
+        public string SearchText { get; set; }
+
+        public string SearchBy { get; set; }
 
         public async Task OnGetAsync()
         {
             Position = await _context.Position.ToListAsync();
+            SearchCriteria = (from t in typeof(Position).GetProperties()
+                              select new SelectListItem { Text = t.Name, Value = t.Name}).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            SearchCriteria = (from t in typeof(Position).GetProperties()
+                              select new SelectListItem { Text = t.Name, Value = t.Name }).ToList();
+
+            Position = await _context.Position.ToListAsync();
+
+            if (SearchText == null)
+            {
+                ModelState.AddModelError("", "Enter Search Criteria");
+            }
+            else if (SearchBy == null)
+            {
+                ModelState.AddModelError("", "Choose Search By");
+            }
+            else
+            {
+                Position = _context.getPositionsByCriteria(SearchBy, SearchText);
+            }
+
+            return Page();
         }
     }
 }
